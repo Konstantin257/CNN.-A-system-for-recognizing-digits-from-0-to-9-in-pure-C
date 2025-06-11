2c77419c5aa297a7858496ffc18a93eae4c14c9e
Konstantin Stepanov <Konstantin257@vk.com>
2025-06-11 13:13:44 +0300

    Initial commit with LFS

---
+++ Степанов К. О/1. Классическое приложение/2. Отрисованные 4 проекта/1. Система распознавания.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:462ae7dfa9181dde11cf778fc17a9c98bb55271ffad41b846d7b271e7acaf4ba
+size 84198
---
+++ Степанов К. О/2. Консольное, только алгоритмы обучения/Консольное, только алгоритмы обучения/Консольное, только алгоритмы обучения.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d311814dbf432ff4dc596c34c10cc6a38cd38eda12cd126699f645a32d7d2534
+size 41459
